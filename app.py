e150b109bdf2633eaabb0c05bc740c401980efb8
Thai Nguyen <thainguyen20594@gmail.com>
2024-12-23 11:20:48 -0500

    Add chatbot to Flask server

--- app.py
+++ app.py
@@ -1,30 +1,42 @@
-# import the Flask class from the flask module.
-from flask import Flask
-from flask_cors import CORS		# newly added
+#flask is module, Flask is class
+from flask import Flask, request, render_template
+from flask_cors import CORS
+import json
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-
+#Initializations 
 app = Flask(__name__)
 CORS(app) #mitigate(reduce) CORS errors - a type of error related to making requests to domains other than the one that hosts this webpage.
 
+model_name = "facebook/blenderbot-400M-distill"
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+conversation_history = []
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/bananas')
-def bananas():
-    return 'This page has bananas!'
+#Implement chatbot function
+@app.route('/chatbot', methods=['POST'])
+def handle_prompt():
+    data = request.get_data(as_text=True)
+    data = json.loads(data)
+    input_text = data['prompt']
     
-@app.route('/bread')
-def bread():
-    return 'This page has bread!'
+    # Create conversation history string
+    history = "\n".join(conversation_history)
 
+    # Tokenize the input text and history
+    inputs = tokenizer.encode_plus(history, input_text, return_tensors="pt")
 
-
-
-
-
-
+    # Generate the response from the model
+    outputs = model.generate(**inputs, max_length= 60)  # max_length will cause the model to crash at some point as history grows
+    
+    # Decode the response
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    
+    # Add interaction to conversation history
+    conversation_history.append(input_text)
+    conversation_history.append(response)
+    return response
+    
 # this ensures that the server is only run if the script is executed directly, not when imported as a module
 if __name__ == '__main__':
     app.run()
